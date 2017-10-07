541f3980441806ed02374bac6c8997eb7262d728
Jesús <30764221+JesMJM@users.noreply.github.com>
2017-10-07 07:30:54 -0400

    Opens files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 
 int main()
 {
+	std::cout << "Enter file name:";
+	std::string fileName;
+	std::cin >> fileName;
+
+	std::fstream narc(fileName, std::ios::binary | std::ios::in);
+
+	if (narc.fail())
+	{
+		std::cerr << "Error opening \"" << fileName << "\"" << std::endl;
+		return 1;
+	}
+
+	std::cin.clear();
+	std::cin.ignore(32767, '\n');
+	std::cin.get();
 }
