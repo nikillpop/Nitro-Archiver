e937b48e2abca89050bd3e0624d85afdfa512779
Jesús <30764221+JesMJM@users.noreply.github.com>
2017-10-08 08:13:15 -0400

    More command handling

--- main.cpp
+++ main.cpp
@@ -2,26 +2,25 @@
 #include <iomanip>
 #include <iostream>
 
-/**
- * Idea para convertir los comandos que el usuario
- * introduzca en "tempCmds" en algo manejable, convirtiendo
- * los comandos en un array de enumeradores
- *
-enum class cmds
+enum class arg
 {
-	NO_CMDS,
+	// Usados como flags
+	NO_ARG,
 	ERROR,
-	HELP,
-	HEADER,
+
+	// Comandos
+	HELP,  //-h
+	HEADER,//-e
 };
-*/
+
+void parseTempArgs(std::string, arg[], const int);
 
 int main()
 {
-	std::cout << "Narc-Archiver, Commit #7, By Jes" << char(163) << "s (Jes)\n"
+	std::cout << "Narc-Archiver, Commit #11, By Jes" << char(163) << "s (Jes)\n"
 	          << std::endl;
 
-	std::cout << "Enter file name: ";
+	std::cout << "Enter filename to open: ";
 	std::string fileName;
 	std::getline(std::cin, fileName);
 
@@ -37,10 +36,27 @@ int main()
 		std::cout << "File opened correctly\n" << std::endl;
 	}
 
-	std::string tempCmds;
+	/** "string TempArgs" guardara los argumentos en línea
+	 * para luego almacenarlos en "arg argument" de una
+	 * manera que se puedan utilizar mejor */
+	std::string tempArgs;
+	std::cout << "Enter arguments: ";
+	std::getline(std::cin, tempArgs);
 
-	std::cout << "Enter commands: ";
-	std::getline(std::cin, tempCmds);
+	/** El numero máximo de argumentos simultáneos
+	 * serán 2 por ahora, luego intentare usar std::array*/
+	const int maxArgs{2};
+	arg arguments[maxArgs]{arg::NO_ARG};
+
+	parseTempArgs(tempArgs, arguments, maxArgs);
 
 	std::cin.get();
+}
+
+void parseTempArgs(std::string argsToParse, arg finalArgs[], const int maxArgs)
+{
+	if (argsToParse.compare(0, 2, "-h") == 0)
+	{
+		finalArgs[0] = arg::HELP;
+	}
 }
