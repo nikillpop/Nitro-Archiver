7c29c2f5aa29239a21e712887bca456d557171c1
Jesús <30764221+JesMJM@users.noreply.github.com>
2017-10-07 21:13:21 -0400

    Commands handling started

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 
 int main()
 {
-	std::cout << "Enter file name:";
+	std::cout << "Narc-Archiver, Commit #7, By Jes" << char(163) << "s (Jes)\n"
+	          << std::endl;
+
+	std::cout << "Enter file name: ";
 	std::string fileName;
 	std::cin >> fileName;
 
@@ -15,9 +18,17 @@ int main()
 		std::cerr << "Error opening \"" << fileName << "\"" << std::endl;
 		return 1;
 	}
+	else
+	{
+		std::cout << "File opened correctly\n" << std::endl;
+	}
 
-	std::cin.clear();
+	std::string cmds;
+
+	std::cout << "Enter commands: ";
+	std::cin.ignore();
+	std::getline(std::cin, cmds);
+
 	std::cin.ignore(32767, '\n');
 	std::cin.get();
 }
-//Weebhook test
