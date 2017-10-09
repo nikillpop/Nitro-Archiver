ae21f6d04bdbd9c92eb7f2bf0f4f4292adc24a99
Jesús <30764221+JesMJM@users.noreply.github.com>
2017-10-09 13:58:32 -0400

    Header read cmd. now reads chunk name

--- commands/header_data.cpp
+++ commands/header_data.cpp
@@ -4,11 +4,24 @@
 
 void readHeaderData(std::fstream &narc)
 {
+	std::cout << std::endl;
+
+	char readedData[4]{};
+
+
+	narc.seekg(0x0, std::ios::beg);
+	narc.read(readedData, 0x4);
+
+	std::cout << "Chunk Name:\t\"";
+	for (auto &letter : readedData)
+	{
+		std::cout << letter;
+	}
+	std::cout << "\"" << std::endl;
+
+
 	narc.seekg(0x18, std::ios::beg);
+	narc.read(readedData, 0x2);
 
-	char read[2];
-
-	narc.read(read, 0x2);
-
-	std::cout << "Files:\t" << static_cast<int>(read[0]) + static_cast<int>(read[1]) << std::endl;
+	std::cout << "Files:\t\t" << static_cast<int>(readedData[0]) + static_cast<int>(readedData[1]) << std::endl;
 }
