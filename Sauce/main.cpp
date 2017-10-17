abdc81168afa4a2dd01f7b1c4e85cebf20cbc0c4
Jesús <30764221+JesMJM@users.noreply.github.com>
2017-10-17 08:33:50 -0400

    Error handling in file input

--- Sauce/main.cpp
+++ Sauce/main.cpp
@@ -15,6 +15,7 @@
  *  along with Nitro-Archiver.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -31,8 +32,9 @@ enum class arg
 };
 
 void parseTempArgs(std::string, arg[], const int);
-void caller(arg[], const int, std::fstream &);
-void readHeaderData(std::fstream &);
+void caller(arg[], const int, std::fstream &narc);
+void readHeaderData(std::fstream &narc);
+void chekFile(std::fstream &narc, bool openedViaArgument);
 
 int main(int argc, char *argv[])
 {
@@ -61,6 +63,8 @@ int main(int argc, char *argv[])
 		else
 		{
 			std::cout << "File opened correctly\n" << std::endl;
+			//False means the file was selected with Nitro-Archiver
+			chekFile(narc, false);
 		}
 	}
 	else if (argc == 2)
@@ -73,6 +77,8 @@ int main(int argc, char *argv[])
 			std::cerr << "Error opening \"" << argv[1] << "\"" << std::endl;
 			return 1;
 		}
+		//True means the file was selected via argument
+		chekFile(narc, true);
 	}
 	else
 	{
@@ -81,15 +87,10 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	/** "string TempArgs" guardara los argumentos en línea
-	 * para luego almacenarlos en "arg argument" de una
-	 * manera que se puedan utilizar mejor */
 	std::string tempArgs;
 	std::cout << "Enter arguments: ";
 	std::getline(std::cin, tempArgs);
 
-	/** El numero máximo de argumentos simultáneos
-	 * serán 2 por ahora, luego intentare usar std::array*/
 	const int maxArgs{2};
 	arg arguments[maxArgs]{arg::NO_ARG};
 
@@ -102,21 +103,60 @@ int main(int argc, char *argv[])
 
 void parseTempArgs(std::string argsToParse, arg finalArgs[], const int maxArgs)
 {
-	if (argsToParse.compare(0, 2, "-h") == 0)
-	{
-		finalArgs[0] = arg::HELP;
-	}
+	if (argsToParse.compare(0, 2, "-h") == 0) finalArgs[0] = arg::HELP;
 
-	if (argsToParse.compare(0, 2, "-d") == 0)
-	{
-		finalArgs[0] = arg::HEADER_DATA;
-	}
+	if (argsToParse.compare(0, 2, "-d") == 0) finalArgs[0] = arg::HEADER_DATA;
 }
 
 void caller(arg arguments[], const int maxArgs, std::fstream &narc)
 {
-	if (arguments[0] == arg::HEADER_DATA)
+	if (arguments[0] == arg::HEADER_DATA) readHeaderData(narc);
+}
+
+void chekFile(std::fstream &narc, bool openedViaArgument)
+{
+	bool invalidInput{false}; //If true at the end of the func, trows a warn
+	char buffer[4]{};         //Temporal storage for using read();
+	std::string tempSting{};  //For comparing buffer with == or !=
+
+	//First 0x4 bytes of narc header must say "NARC"
+	narc.seekg(0x0, std::ios::beg);
+	narc.read(buffer, 0x4);
+	tempSting.assign(buffer, 4);
+	if (tempSting != "NARC") invalidInput = true;
+
+	//First 0x4 bytes of FAT must say "BTAF"
+	narc.seekg(0x10, std::ios::beg);
+	narc.read(buffer, 0x4);
+	tempSting.assign(buffer, 4);
+	if (tempSting != "BTAF") invalidInput = true;
+
+	//The FAT chunk size must make sense whit the number of files
+	uint16_t files{};
+	uint32_t chunkSize{};
+
+	narc.seekg(0x18, std::ios::beg);
+	narc.read(buffer, 0x2);
+	files = buffer[1] * 256 + buffer[0];
+
+	narc.seekg(0x14, std::ios::beg);
+	narc.read(buffer, 0x4);
+	chunkSize = buffer[3] * 256 * 256 * 256 + buffer[2] * 256 * 256 +
+	            buffer[1] * 256 + buffer[0];
+
+	if (((files * 8u) + 0xCu) != chunkSize) invalidInput = true;
+
+	//End of checks
+	if (invalidInput)
 	{
-		readHeaderData(narc);
+		std::cout << "Error:\tThe file is invalid or is corrupted\n"
+		          << "\tyou continue at your own risk.\n\n";
+
+		if (openedViaArgument)
+		{
+			std::cout << "Press enter to continue.";
+			if (std::cin.get() == '\n') return;
+			exit(0);
+		}
 	}
 }
